48a25a7bb6f3326bc28fccabe0a986295597fb98
Towhidur Rahman <towhidasif@gmail.com>
2025-09-02 01:38:21 +0600

    update

---
+++ api/index.js
@@ -0,0 +1,7 @@
+// File: /psycare/api/index.js
+
+// This line imports the configured Express app from your backend folder
+import app from '../backend/src/app.js';
+
+// This line exports the app for Vercel to use
+export default app;
